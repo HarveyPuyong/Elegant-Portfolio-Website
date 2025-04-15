801987b78b1c173a5650587e59c05b875075b9c8
harvey puyong <harveyluslos80@gmail.com>
2025-04-15 11:15:25 +0800

    Update the footer height when resize the document

--- script/main.js
+++ script/main.js
@@ -1,2 +1,8 @@
-const screenHeight =  window.innerHeight;
-const footer = document.querySelector('footer').style.height = `${screenHeight}px`
+function updateFooterHeight() {
+  const screenHeight =  window.innerHeight;
+  console.log(screenHeight);
+  const footer = document.querySelector('footer').style.height = `${screenHeight}px`;  
+}
+
+updateFooterHeight();
+window.addEventListener('resize', updateFooterHeight);
